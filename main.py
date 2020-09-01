2f2b20d6d1e127e11542931e8b9f81faa4e50869
eim5178 <eim5178@psu.edu>
2020-09-01 19:39:04 +0000

    spacing and outputs

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ temperature = int(input("Enter temperature: "))
 response = input("Enter unit in F/f or C/c: ")
 if response == 'F' or response == 'f' :
   fahrenheit = (temperature - 32) * 5/9
-  print (f"{temperature}\N{degree sign} in fahrenheit is equivalent to {fahrenheit}\N{degree sign} celsius.")
+  print (f"{temperature}\N{degree sign} in Fahrenheit is equivalent to {fahrenheit}\N{degree sign} Celsius.")
 elif response == 'C' or response == 'c':
   celsius = (temperature * 9/5) + 32
-  print (f"{temperature}\N{degree sign} in celsius is equivalent to {celsius}\N{degree sign} fahrenheit.")
+  print (f"{temperature}\N{degree sign} in Celsius is equivalent to {celsius}\N{degree sign} Fahrenheit.")
 else:
-  print ("Invalid unit(bad).")
+  print ("Invalid unit({response}).")
